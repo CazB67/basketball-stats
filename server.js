0c32a7fd44365f82195abb1a229fd4c76a170ae4
Caroline <caroline.portilla@gmail.com>
2020-07-17 23:24:14 +0800

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+//const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/stats", { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Add routes, both API and view
-app.use(routes);
+//app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
